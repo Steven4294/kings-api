0bd202d7d497f4ff0e0f8f340147aaed6966f375
Steven Petteruti <stevenpetterutijr@gmail.com>
2021-02-20 18:06:49 -0500

    remove tests

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,14 +6,31 @@ import Vapor
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
     app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+        hostname: "ec2-18-204-101-137.compute-1.amazonaws.com",
+        port: 5432,
+        username: "syjrqiibgtpetx",
+        password: "d2aee37e4b245940afa506116bcd9c8309d647b97298c69cf4205a300c7c4183",
+        database: "d9jpcv88iknnqd"
     ), as: .psql)
+    
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all,
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin]
+    )
+    
+    let cors = CORSMiddleware(configuration: corsConfiguration)
+    
+    let error = ErrorMiddleware.default(environment: app.environment)
+    
+    // Clear any existing middleware.
+    app.middleware = .init()
+    app.middleware.use(cors)
+    app.middleware.use(error)
+    
+    
+
 
     app.migrations.add(CreateTodo())
 
